ca47b021e3d9c13b4e400e25ea4756d46118dabb
Logan Hasson <logan.hasson@gmail.com>
2016-01-20 22:29:36 -0500

    Process async

--- web/controllers/event_controller.ex
+++ web/controllers/event_controller.ex
@@ -2,14 +2,16 @@ defmodule PhoenixPushstream.EventController do
   use PhoenixPushstream.Web, :controller
 
   def create(conn, params) do
-    id   = params["id"]
-    type = params["type"]
+    Task.async(fn ->
+      id   = params["id"]
+      type = params["type"]
 
-    event_params = params
-                   |> Map.delete("id")
-                   |> Map.delete("type")
+      event_params = params
+                     |> Map.delete("id")
+                     |> Map.delete("type")
 
-    PhoenixPushstream.Endpoint.broadcast("events:#{id}", type, event_params)
+      PhoenixPushstream.Endpoint.broadcast("events:#{id}", type, event_params)
+    end)
 
     conn
     |> json(%{message: "success"})
